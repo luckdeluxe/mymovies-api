fdf6d5b13527787a0f5c5b4380a6ece98ccd0548
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-26 15:28:22 -0300

    Add movie paths

--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+from films import views as film_views
 
 #Api Router
 router = routers.DefaultRouter()
-
+router.register('films', film_views.FilmViewSet, basename='Film')
+router.register('genres', film_views.GenreViewSet, basename='FilmGenre')
 
 urlpatterns = [
     #Admin Routes
@@ -13,6 +17,8 @@ urlpatterns = [
     #Api routes
     path('api/', include('authentication.urls')),
     path('api/', include(router.urls)),
+] 
 
-
-]
+# Serve static files in development server
+if settings.DEBUG:
+    urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
