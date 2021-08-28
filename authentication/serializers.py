0340c0d85cd6a1439ba0d82ddd35e5a494d85fc1
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-28 14:02:05 -0300

    Add email and user validations

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,10 +9,34 @@ class UserSerializer(serializers.ModelSerializer):
         required=True)
     password = serializers.CharField(
         min_length=8, write_only=True)
-
+    avatar = serializers.ImageField(
+        required=False, allow_null=True)
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username', 'password')
+        fields = ('email', 'username', 'password', 'avatar')
 
     def validate_password(self, value):
         return make_password(value)
+
+    def validate_username(self, value):
+        value = value.replace(" ", "")  #Since we are we erase the spaces
+        try:
+            user = get_user_model().objects.get(username=value)
+            #If it is the same user by sending the same username, we will let you
+            if user == self.instance:
+                return value
+        except get_user_model().DoesNotExist:
+            return value
+        raise serializers.ValidationError("Username in use")
+    def validate_email(self, value):
+        #Is there a user with this email already registered?
+        try:
+            user = get_user_model().objects.get(email=value)
+        except get_user_model().DoesNotExist:
+            return value
+        #In any other case the validation will fail
+        raise serializers.ValidationError("Email in use")
+
+    def update(self, instance, validated_data):
+        validated_data.pop('email', None)               #We prevent erasure
+        return super().update(instance, validated_data)  #We follow the execution
