3693f81f76dc1d0f97a82d377da87d3dccb7d81c
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-28 13:57:36 -0300

    Update profile model adding avatar

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+def path_to_avatar(instance, filename):                   
+    return f'avatars/{instance.id}/{filename}'
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=50, unique=True)
-
+    avatar = models.ImageField(upload_to=path_to_avatar, null=True, blank=True)
 
 USERNAME_FIELD = 'email'
 REQUIRED_FIELDS = ['username', 'password']
